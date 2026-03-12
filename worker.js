84dfb910e8da835082971408844c815822ed2f54
MBP13 <mbp13@chaoti.info>
2026-03-12 22:37:53 +0900

    refactor: Simplify worker.js fetch logic and update wrangler.toml configuration

--- worker.js
+++ worker.js
@@ -1,12 +1,5 @@
-import htmlString from './index.html';
-
 export default {
-  async fetch(request, env, ctx) {
-    return new Response(htmlString, {
-      headers: {
-        'Content-Type': 'text/html; charset=utf-8',
-        'Cache-Control': 'public, max-age=3600'
-      },
-    });
+  async fetch(request, env) {
+    return env.ASSETS.fetch(request);
   },
 };
